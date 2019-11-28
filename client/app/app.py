6eafff0518187ebd2b3dca86cd4e9eccc0d8ec46
Lucky <lucky_soft@163.com>
2019-11-28 19:46:05 +0800

    feature: add export excel

--- client/app/app.py
+++ client/app/app.py
@@ -2,45 +2,40 @@ import wx
 import os, shutil
 import pandas as pd
 
-class MyTextDropTarget(wx.TextDropTarget):
-    def __init__(self, object):
-        wx.TextDropTarget.__init__(self)
-        self.object = object
-
-    def OnDropText(self, x, y, data):
-        self.object.InsertStringItem(0, data)
-
-
-class MyFrame(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(800, 600))
-
-        splitter1 = wx.SplitterWindow(self, -1)
-        splitter2 = wx.SplitterWindow(splitter1)
-        splitter3 = wx.SplitterWindow(splitter2)
-        self.dir = wx.GenericDirCtrl(splitter1,
+class ExportPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size = (800, 600))
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        pnl1 = wx.Panel(self, -1, style = wx.SIMPLE_BORDER)
+        pnl2 = wx.Panel(self, -1, style = wx.SIMPLE_BORDER)
+        pnl3 = wx.Panel(self, -1, style = wx.SIMPLE_BORDER)
+        hbox.Add(pnl1, 1, wx.EXPAND|wx.ALL, 3)
+        hbox.Add(pnl2, 1, wx.EXPAND|wx.ALL, 3)
+        hbox.Add(pnl3, 1, wx.EXPAND|wx.ALL, 3)
+        self.SetSize((400, 500))
+        self.SetSizer(hbox)
+        self.dir = wx.GenericDirCtrl(pnl1,
                                      -1,
+                                     size = (200, 600),
                                      dir='/home/',
                                      style=wx.DIRCTRL_DIR_ONLY
                                      | wx.BORDER_RAISED)
         tree = self.dir.GetTreeCtrl()
         wx.EVT_TREE_SEL_CHANGED(self, tree.GetId(), self.OnSelect)
-        self.lc1 = wx.ListCtrl(splitter2, -1, style=wx.LC_LIST)
+        self.lc1 = wx.ListCtrl(pnl2, -1, size = (200, 600), style=wx.LC_LIST)
         wx.EVT_LIST_BEGIN_DRAG(self, self.lc1.GetId(), self.OnDragInit)
         tree = self.dir.GetTreeCtrl()
-        splitter1.SplitVertically(self.dir, splitter2, 200)
-        splitter2.SplitVertically(self.lc1, splitter3, 200)
-        self.textKey = wx.StaticText(splitter3, -1, 'Total: ', (20, 60))
-        self.textValue = wx.StaticText(splitter3, -1, 'Total: ', (20, 80))
-        self.picNum = wx.TextCtrl(splitter3,
+        self.textKey = wx.StaticText(pnl3, -1, 'Total: ', (20, 60))
+        self.textValue = wx.StaticText(pnl3, -1, 'Total: ', (20, 80))
+        self.picNum = wx.TextCtrl(pnl3,
                                   -1,
                                   'Export Number', (80, 60),
                                   size=(160, 30))
-        self.exportBtn = wx.Button(splitter3, -1, 'Generate', (250, 60))
+        self.exportBtn = wx.Button(pnl3, -1, 'Generate', (250, 60))
         self.exportBtn.Bind(wx.EVT_BUTTON, self.export)
-        self.file_name_key = wx.StaticText(splitter3, -1, 'Filename',
+        self.file_name_key = wx.StaticText(pnl3, -1, 'Filename',
                                            (60, 120))
-        self.file_name_value = wx.TextCtrl(splitter3,
+        self.file_name_value = wx.TextCtrl(pnl3,
                                            -1,
                                            'Custom Filename', (120, 120),
                                            size=(160, 30))
@@ -52,7 +47,6 @@ class MyFrame(wx.Frame):
         list = os.listdir(self.dir.GetPath())
         self.lc1.ClearAll()
         self.textValue.SetLabel(str(len(list)))
-        print(len(list), ' ----------------- len list ---------------')
         for i in range(len(list)):
             if list[i][0] != '.':
                 self.lc1.InsertItem(0, list[i])
@@ -71,23 +65,91 @@ class MyFrame(wx.Frame):
         file_num = self.picNum.GetValue()
         
         if os.path.exists(self.dir.GetPath() + '/' + 'result.xlsx'):
-            
-            
-            # print(excel_file_name['Filename'], ' --------------- file name--------------')
-            
-                    
-        else:
-            os.mknod(self.dir.GetPath() + '/' + 'result.xlsx')
-            df1 = pd.DataFrame({'Filename': list, '': None, 'Status': None})
-            
-            excel_file_name = pd.read_excel(self.dir.GetPath() + '/' + 'result.xlsx')
+            df = pd.read_excel(self.dir.GetPath() + '/' + 'result.xlsx')
             for file in range(int(file_num)):
                 shutil.move(self.dir.GetPath() + '/' + list[file], self.dir.GetPath() + '/' + export_file_name)
-                if  list[file] in excel_file_name['Filename']:
-                    df1.loc[df1[df1['Filename'].isin([list[file]])][['Status']].index[0]]['Status'] = 'Sending'
-                    
+                for j in range(len(df['Filename'])):
+                    if list[file] == df['Filename'][j]:
+                        df['Status'][j] = 'Sending'
+            df.to_excel(self.dir.GetPath() + '/' + 'result.xlsx', sheet_name = 'Sheet1', index = False)
+        else:
+            df1 = pd.DataFrame({'Filename': list, '': None, 'Status': None})
+            for file in range(int(file_num)):
+                shutil.move(self.dir.GetPath() + '/' + list[file], self.dir.GetPath() + '/' + export_file_name)
+                df1.loc[file]['Status'] = 'Sending'
+                df1.to_excel(self.dir.GetPath() + '/' + 'result.xlsx', sheet_name = 'Sheet1', index = True)
 
+class MergePanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size = (800, 600))
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        pnl1 = wx.Panel(self, -1, style = wx.SIMPLE_BORDER)
+        pnl2 = wx.Panel(self, -1, style = wx.SIMPLE_BORDER)
+        pnl3 = wx.Panel(self, -1, style = wx.SIMPLE_BORDER)
+        hbox.Add(pnl1, 1, wx.EXPAND|wx.ALL, 3)
+        hbox.Add(pnl2, 1, wx.EXPAND|wx.ALL, 3)
+        hbox.Add(pnl3, 1, wx.EXPAND|wx.ALL, 3)
+        self.SetSize((400, 500))
+        self.SetSizer(hbox)
 
+        self.dir1 = wx.GenericDirCtrl(pnl1,
+                                     -1,
+                                     size = (200, 600),
+                                     dir='/home/',
+                                     style=wx.DIRCTRL_DIR_ONLY
+                                     | wx.BORDER_RAISED)
+        self.dir2 = wx.GenericDirCtrl(pnl2,
+                                     -1,
+                                     size = (200, 600),
+                                     dir='/home/',
+                                     style=wx.DIRCTRL_DIR_ONLY
+                                     | wx.BORDER_RAISED)
+
+        self.mergeBtn = wx.Button(pnl3, -1, 'Merge', (20, 20))
+        self.mergeBtn.Bind(wx.EVT_BUTTON, self.OnMerge)
+
+    def OnMerge(self, event):
+        xml_list = []
+        img_list = []
+        os.mkdir(self.dir1.GetPath() + '/result')
+        for home, dirs, files in os.walk(self.dir1.GetPath()):
+            for file in files:
+                if os.path.splitext(file)[1] == '.jpg' or os.path.splitext(file)[1] == '.png':
+                    img_list.append(file)
+        for home, dirs, files in os.walk(self.dir2.GetPath()):
+            for file in files:
+                print(os.path.join(home, file),' ------------- file ---------------')
+                if os.path.splitext(file)[1] == '.xml':
+                    xml_list.append(file)
+        for i in xml_list:
+            for j in img_list:
+                if os.path.splitext(i)[0] == os.path.splitext(j)[0]:
+                    shutil.move(self.dir1.GetPath() + '/' + j, self.dir1.GetPath() + '/result')
+                    # shutil.copy()
+
+        print(len(xml_list), ' ---------------xml list------------')
+        print(len(img_list), ' ---------------img list------------')
+
+class MyFrame(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title, size=(800, 600))
+
+        p = wx.Panel(self)
+        nb = wx.Notebook(p)
+
+        tab1 = ExportPanel(nb)
+        tab2 = MergePanel(nb)
+
+        nb.AddPage(tab1, 'tab1')
+        nb.AddPage(tab2, 'tab2')
+
+        sizer = wx.BoxSizer()
+        sizer.Add(nb, 1, wx.EXPAND)
+        p.SetSizer(sizer)
+
+        
+
+    
 class MyApp(wx.App):
     def OnInit(self):
         frame = MyFrame(None, -1, 'Client')
